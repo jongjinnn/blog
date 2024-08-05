7b8044d2fec176812281e6f4e88093dc2608c0a1
jongjinnn <jongjin.me@gmail.com>
2024-08-05 17:15:10 +0900

    first commit

---
+++ pages/index.js
@@ -0,0 +1,42 @@
+import Head from 'next/head'
+import Layout, { siteTitle } from '../components/Layout'
+import { getSortedPostData } from '../lib/posts'
+import utilStyles from '../styles/utils.module.css'
+import Link from 'next/link'
+import Date from '../components/Date'
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostData()
+  return {
+    props: {
+      allPostsData,
+    },
+  }
+}
+
+export default function Home({ allPostsData }) {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>🤟 Learning NextJS</p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>{title}</Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
+  )
+}
